8f80a56eca0e7c89f70020f69dba0e89a15df2a1
David Van Duzer <dvd@tennica.net>
2020-07-01 09:08:04 -0600

    enable server build

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 provider "digitalocean" {
 }
 
-
+module "server-droplet" {
+  source = "./modules/server-droplet"
+  ssh_fingerprint = "${var.ssh_fingerprint}"
+  server_count = "${var.server_count}"
+}
